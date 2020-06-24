42923e341a23aeaf8b651cb705125c77118a869f
yMatinho <matheusmaceio2013@gmail.com>
2020-06-24 17:26:45 -0300

    Delivery 1.0

---
+++ pages/registrar.php
@@ -0,0 +1,70 @@
+<section class="registrar">
+	<?php
+		use Database\MySql;
+		use App\Site;
+		if(isset($_POST['acao'])) {
+			$usuario = $_POST['usuario'];
+			$nome = $_POST['nome'];
+			$senha = $_POST['senha'];
+			$confirmar_senha = $_POST['confirmar-senha'];
+			$email = $_POST['email'];
+			$numero = $_POST['numero'];
+			$imagem = '';
+			try {
+				if(empty($usuario) || empty($nome) || empty($senha) || empty($email) || empty($numero))
+					throw new Exception('Campos vazios não são permitidos!');
+				if($senha != $confirmar_senha)
+					throw new Exception('Senhas não correspondem!');
+				if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+					throw new Exception('Email inválido!');
+				$validar_usuario = MySql::select('tb_admin.usuarios', 'WHERE usuario = ?', array($usuario));
+				if($validar_usuario->rowCount() >= 1)
+					throw new Exception('Usuário já existe!');
+				$validar_email = MySql::select('tb_admin.usuarios', 'WHERE email = ?', array($email));
+				if($validar_email->rowCount() >= 1)
+					throw new Exception("Email já existe!");
+				$validar_numero = MySql::select('tb_admin.usuarios', 'WHERE numero = ?', array($numero));
+				if($validar_numero->rowCount() >= 1)
+					throw new Exception("Número já existe!");
+				$sql = MySql::connect()->prepare("INSERT INTO `tb_admin.usuarios` VALUES(null,?,?,?,?,?,?,?)");
+				$sql->execute(array($usuario, $senha, $email, $numero, $nome, $imagem, 0));
+				Site::redirect(INCLUDE_PATH.'login');
+			} catch(Exception $e) {
+				Site::alert('error', $e->getMessage());
+			}
+		}
+	?>
+	<h2>Registrar-se</h2>
+	<form method="post">
+		<div class="form-group">
+			<label>Usuário:</label>
+			<input type="text" name="usuario">
+		</div>
+		<div class="form-group">
+			<label>Nome:</label>
+			<input type="text" name="nome">
+		</div>
+		<div class="form-group">
+			<label>Senha:</label>
+			<input type="password" name="senha">
+		</div>
+		<div class="form-group">
+			<label>Confirmar Senha:</label>
+			<input type="password" name="confirmar-senha">
+		</div>
+		<div class="form-group">
+			<label>E-mail:</label>
+			<input type="text" name="email">
+		</div>
+		<div class="form-group">
+			<label>Número de Telefone</label>
+			<input type="text" name="numero">
+		</div>
+		<div class="form-group">
+			<input type="submit" name="acao" value="Registrar">
+		</div>
+	</form>
+</section>
+<script type="text/javascript">
+	$('input[name=numero]').mask('99999999999');
+</script>
---
+++ painel/pages/cadastrar-slide.php
@@ -0,0 +1,36 @@
+<?php
+use Database\MySql;
+use App\Site;
+?>
+<div class="title">
+	<i class="fas fa-image"></i> <span>Cadastrar Slide</span>
+</div>
+<?php
+	if(isset($_POST['acao'])) {
+		try {
+			$imagem = $_FILES['imagem'];
+			$info = $_POST['info'];
+			if(empty($info) || empty($imagem))
+				throw new Exception('Campos vazios não são permitidos!');
+			if(!Site::imagemValida($imagem))
+				throw new Exception('Imagem inválida!');
+			$imagem = Site::uploadFile($imagem);
+			$order_id = MySql::find('tb_site.slides', 'ORDER BY order_id DESC LIMIT 1');
+			if(count($order_id) == 0)
+				$order_id = 1;
+			else
+				$order_id = $order_id['order_id'] + 1;
+			MySql::insert('tb_site.slides', array($order_id, $imagem, $info));
+		}
+	}
+?>
+<form enctype="multipart/form-data" method="post">
+	<div class="form-group">
+		<label>Imagem:</label>
+		<input type="file" name="imagem">
+	</div>
+	<div class="form-group">
+		<label>HTML:</label>
+		<textarea name="info"></textarea>
+	</div>
+</form>
